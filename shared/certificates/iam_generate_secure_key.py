fd9552f8a68d550317042826629d1881ecb04e80
Ross Porter <rossporter506@gmail.com>
2024-02-13 16:12:04 +1300

    Synchronise TEE key with Python code
    The TEE was using a different key to the Python implementation.

--- shared/certificates/iam_generate_secure_key.py
+++ shared/certificates/iam_generate_secure_key.py
@@ -1,13 +1,17 @@
-from ecdsa import VerifyingKey, SigningKey
+from ecdsa import VerifyingKey, SigningKey, Ed25519
 import os
 
 if __name__ == "__main__":
     try:
-        os.makedirs(folder_path)
+        os.makedirs("IAM_keys")
     except FileExistsError:
         # Ignore the error if the folder already exists
         pass
-    secc_secure_key = SigningKey.generate()
+    #secc_secure_key = SigningKey.generate(curve=Ed25519)
+    with open("IAM_TEE_key.der", "rb") as f:
+        der_string = f.read()
+    
+    secc_secure_key = SigningKey.from_der(der_string)
     secc_public_key = secc_secure_key.verifying_key
     with open("IAM_keys/secc_private_attestation_key.pem", "wb") as f:
         f.write(secc_secure_key.to_pem(format="pkcs8"))
