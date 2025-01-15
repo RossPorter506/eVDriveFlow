5c564ec835c64bf663e001714bf915d9fa3384d5
Ross Porter <rossporter506@gmail.com>
2025-01-15 16:58:50 +1300

    More bugfixes

--- secc/evse_session.py
+++ secc/evse_session.py
@@ -72,7 +72,7 @@ class EVSESession(CommunicationSession):
         welding_detection_state = ProcessDcWeldingDetectionRequest()
         dc_charge_loop_state = ProcessDcChargeLoopRequest()
         session_stop_state = ProcessSessionStopRequest()
-        states = [supported_app_protocol_state, session_setup_state, authorization_setup_state, tpm_capability_challenge_state, tpm_evcc_attest_state, authorization_state,
+        states = [supported_app_protocol_state, session_setup_state, authorization_setup_state, tpm_capability_challenge_state, tpm_capability_evidence_state, authorization_state,
                   service_discovery_state, service_detail_state, service_selection_state,
                   charge_parameter_discovery_state, attestation_state, schedule_exchange_state, cable_check_state, pre_charge_state,
                   welding_detection_state, power_delivery_state, dc_charge_loop_state, session_stop_state]
