df7ba9bc77264e6acd5c96df21b9b93d0472c7ff
Ross Porter <rossporter506@gmail.com>
2025-02-03 13:47:34 +1300

    New certs
    Forgot to add logic to generate the EVCC attestation key

--- shared/certificates/iam_generate_secure_key.py
+++ shared/certificates/iam_generate_secure_key.py
@@ -7,13 +7,41 @@ if __name__ == "__main__":
     except FileExistsError:
         # Ignore the error if the folder already exists
         pass
-    #secc_secure_key = SigningKey.generate(curve=Ed25519)
-    with open("IAM_TEE_key.der", "rb") as f:
-        der_string = f.read()
     
-    secc_secure_key = SigningKey.from_der(der_string)
+    # SECC
+    try:
+        with open("IAM_keys/IAM_TEE_key_secc.der", "rb") as f:
+            print("Reading existing SECC key...")
+            secc_der_string = f.read()
+            secc_secure_key = SigningKey.from_der(secc_der_string)
+    except FileNotFoundError:
+        print("Generating new SECC key...")
+        secc_secure_key = SigningKey.generate(curve=Ed25519)
+        secc_secure_key_der = secc_secure_key.to_der(format="pkcs8")
+        with open("IAM_keys/IAM_TEE_key_secc.der", "wb") as f:
+            f.write(secc_secure_key_der)
     secc_public_key = secc_secure_key.verifying_key
+    
     with open("IAM_keys/secc_private_attestation_key.pem", "wb") as f:
         f.write(secc_secure_key.to_pem(format="pkcs8"))
     with open("IAM_keys/secc_public_attestation_key.pem", "wb") as f:
         f.write(secc_public_key.to_pem())
+    
+    # EVCC
+    try:
+        with open("IAM_keys/IAM_TEE_key_evcc.der", "rb") as f:
+            print("Reading existing EVCC key...")
+            evcc_der_string = f.read()
+            evcc_secure_key = SigningKey.from_der(evcc_der_string)
+    except FileNotFoundError:
+        print("Generating new EVCC key...")
+        evcc_secure_key = SigningKey.generate(curve=Ed25519)
+        evcc_secure_key_der = evcc_secure_key.to_der(format="pkcs8")
+        with open("IAM_keys/IAM_TEE_key_evcc.der", "wb") as f:
+            f.write(evcc_secure_key_der)
+    evcc_public_key = evcc_secure_key.verifying_key
+    
+    with open("IAM_keys/evcc_private_attestation_key.pem", "wb") as f:
+        f.write(evcc_secure_key.to_pem(format="pkcs8"))
+    with open("IAM_keys/evcc_public_attestation_key.pem", "wb") as f:
+        f.write(evcc_public_key.to_pem())
